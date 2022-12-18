f1138b84420fd69ac52ef3eeb3c05ced4ec8f8ad
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-12-17 17:17:36 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * This function does something cool
+ * @param {number} param1 - Is somehow useful
+ * @param {number} param2 - Is also somehow useful
+ */
+function doSomethingCool(param1, param2) {}
